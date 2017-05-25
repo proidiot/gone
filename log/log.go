24838b70acfcb58e794ef04acb558125e439d4b0
Charles Southerland <charlie@stuphlabs.com>
2017-05-24 21:15:19 -0500

    Scratch file as of 59263E5B

--- log/log.go
+++ log/log.go
@@ -5,176 +5,223 @@ import (
 	"github.com/proidiot/gone/errors"
 	"log/syslog"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
-type logData struct {
-	pri int
-	m string
-}
-
-type logSettings struct {
-	pid bool
-	cons bool
-	delay bool
-
-}
-
-var log chan<- *logData
-var logControl chan<- *logSettings
-var syslogger *syslog.Writer
-
-// Flags
-var savedPid = false // C default for perror, but log/syslog always sends PID
-var savedCons = false // C default
-var savedPerror = false // C default
-
-// Stored Openlog arguments (in case of delay flag)
-var savedIdent = os.Args[0] // C default
-var savedFacility = syslog.LOG_USER // C default
-
 const (
-	// Log levels
-	LOG_EMERG = 0
-	LOG_ALERT = 1
-	LOG_CRIT = 2
-	LOG_ERR = 3
-	LOG_WARNING = 4
-	LOG_NOTICE = 5
-	LOG_INFO = 6
-	LOG_DEBUG = 7
-
-	// Log facilities
-	LOG_KERN = (0<<3)
-	LOG_USER = (1<<3)
-	LOG_MAIL = (2<<3)
-	LOG_DAEMON = (3<<3)
-	LOG_AUTH = (4<<3)
-	LOG_SYSLOG = (5<<3)
-	LOG_LPR = (6<<3)
-	LOG_NEWS = (7<<3)
-	LOG_UUCP = (8<<3)
-	LOG_CRON = (9<<3)
-	LOG_AUTHPRIV = (10<<3)
-	LOG_FTP = (11<<3)
-	LOG_NTP = (12<<3)
-	LOG_LOG_AUDIT = (13<<3)
-	LOG_LOG_ALERT = (14<<3)
-	LOG_CLOCKD = (15<<3)
-	LOG_LOCAL0 = (16<<3)
-	LOG_LOCAL1 = (17<<3)
-	LOG_LOCAL2 = (18<<3)
-	LOG_LOCAL3 = (19<<3)
-	LOG_LOCAL4 = (20<<3)
-	LOG_LOCAL5 = (21<<3)
-	LOG_LOCAL6 = (22<<3)
-	LOG_LOCAL7 = (23<<3)
-
-	// Log options
-	LOG_PID = 0x01
-	LOG_CONS = 0x02
-	LOG_ODELAY = 0x04
-	LOG_NDELAY = 0x08
-	LOG_NOWAIT = 0x10
-	LOG_PERROR = 0x20
-
 	// local options
 	defaultFacility = syslog.LOG_USER // Default syslog facility in C.
 )
 
-// Openlog 
-func Openlog(ident string, option, facility int) (error) {
-	var e error
-
-	savedPid = (option & LOG_PID) != 0
-	savedCons = (option & LOG_CONS) != 0
-	savedPerror = (option & LOG_PERROR) != 0
-
-	if (option & LOG_ODELAY) != 0 && (option & LOG_NDELAY) != 0 {
-		e = errors.New(
-			"LOG_ODELAY and LOG_NDELAY are both passed to" +
-			"Openlog, but these options are mutually exclusive.",
-		)
-
-		if savedCons {
-			os.Stderr.Write([]byte(e.Error()))
-		}
-	}
-	delay := (option & LOG_NDELAY) == 0
-
-	savedIdent = ident
-
-	// Unary bitwise xor in golang is bitwise complement,
-	// so ^7 is all 1's except the least 3 bits.
-	facilityMask := ^7
-
-	// Unfortunately, log/syslog is missing some facility codes according
-	// to RFC5424. As such, this ugly switch is needed.
-	switch facility & facilityMask {
-	case LOG_KERN:
-		savedFacility = syslog.LOG_KERN
-	case LOG_USER:
-		savedFacility = syslog.LOG_USER
-	case LOG_MAIL:
-		savedFacility = syslog.LOG_MAIL
-	case LOG_DAEMON:
-		savedFacility = syslog.LOG_DAEMON
-	case LOG_AUTH:
-		savedFacility = syslog.LOG_AUTH
-	case LOG_SYSLOG:
-		savedFacility = syslog.LOG_SYSLOG
-	case LOG_LPR:
-		savedFacility = syslog.LOG_LPR
-	case LOG_NEWS:
-		savedFacility = syslog.LOG_NEWS
-	case LOG_UUCP:
-		savedFacility = syslog.LOG_UUCP
-	case LOG_CRON:
-		savedFacility = syslog.LOG_CRON
-	case LOG_AUTHPRIV:
-		savedFacility = syslog.LOG_AUTHPRIV
-	case LOG_FTP:
-		savedFacility = syslog.LOG_FTP
-	case LOG_NTP:
-		savedFacility = syslog.LOG_DAEMON // missing, least bad alt.
-	case LOG_LOG_AUDIT:
-		savedFacility = syslog.LOG_AUTHPRIV // missing, least bad alt.
-	case LOG_LOG_ALERT:
-		savedFacility = syslog.LOG_USER // missing, least bad alt?
-	case LOG_CLOCKD:
-		savedFacility = syslog.LOG_CRON // missing, least bad alt.
-	case LOG_LOCAL0:
-		savedFacility = syslog.LOG_LOCAL0
-	case LOG_LOCAL1:
-		savedFacility = syslog.LOG_LOCAL1
-	case LOG_LOCAL2:
-		savedFacility = syslog.LOG_LOCAL2
-	case LOG_LOCAL3:
-		savedFacility = syslog.LOG_LOCAL3
-	case LOG_LOCAL4:
-		savedFacility = syslog.LOG_LOCAL4
-	case LOG_LOCAL5:
-		savedFacility = syslog.LOG_LOCAL5
-	case LOG_LOCAL6:
-		savedFacility = syslog.LOG_LOCAL6
-	case LOG_LOCAL7:
-		savedFacility = syslog.LOG_LOCAL7
-	default:
-		savedFacility = syslog.LOG_USER // C default rather than error
-	}
-
-	if !delay {
-		syslogger, e = syslog.New(savedFacility, savedIdent)
-
-		if savedCons {
-			os.Stderr.Write([]byte(e.Error()))
-		}
-	}
-
-	return e
+type logData struct {
+	p byte
+	m string
 }
 
-func Syslog(priority int, m string) (error) {
+type Settings struct {
+	Dial        bool     // affects writer
+	Network     string   // affects writer
+	Raddr       string   // affects writer
+	Appname     string   // affects logger
+	Facility    Facility // affects logger
+	Ident       string   // affects logger
+	Pid         bool     // affects logger
+	Cons        bool     // affects writer
+	NoDelay     bool     // affects writer
+	NoWait      bool     // ?
+	Perror      bool     // affects writer
+	NoFallback  bool     // affects writer
+	Reestablish bool     // affects writer
+}
+
+type syslogger struct {
+	c       chan *logData
+	s       Settings
+	w       *syslog.Writer
+	l       *sync.RWMutex
+	console *os.File
+	appname string
+}
+
+var log syslogger
+
+// Stored Openlog arguments (in case of delay flag)
+var savedIdent = os.Args[0]         // C default
+var savedFacility = syslog.LOG_USER // C default
+
+// Openlog
+func Openlog(ident string, option Option, facility Facility) error {
+	if (option&LOG_ODELAY) != 0 && (option&LOG_NDELAY) != 0 {
+		e := errors.New(
+			"LOG_ODELAY and LOG_NDELAY are both being passed to" +
+				"Openlog, but these options are mutually exclusive.",
+		)
+
+		Syslog(LOG_ERR, e.Error())
+
+		return e
+	} else if facility > (LOG_LOCAL7 + Facility(LOG_DEBUG)) {
+		e := errors.New(
+			"Openlog expects the facility argument to be one of" +
+				" the 24 syslog facility constants, but some other" +
+				" value has been specified fo a call to Openlog.",
+		)
+
+		Syslog(LOG_ERR, e.Error())
+
+		return e
+	} else if (facility & Facility(SeverityMask)) != 0 {
+		e := errors.New(
+			"Openlog does not accept a combined facility and" +
+				" severity value for its facility argument even" +
+				" though Syslog does accept such a combined value" +
+				" for its priority argument, but such a combined" +
+				" value has been given for a call to Openlog.",
+		)
+
+		Syslog(LOG_ERR, e.Error())
+
+		return e
+	} else if (option & OptionsMask) != option {
+		e := errors.New(
+			"Openlog expects its option argument to be the" +
+				" bitwise OR of known option flags, but other bits" +
+				" were set in the option argument for a call to" +
+				" Openlog.",
+		)
+
+		Syslog(LOG_ERR, e.Error())
+
+		return e
+	}
+
+	s := Settings{
+		Facility:    byte(facility & FacilityMask),
+		Ident:       ident,
+		Pid:         (option & LOG_PID) != 0,
+		Cons:        (option & LOG_CONS) != 0,
+		NoDelay:     (option & LOG_NDELAY) != 0,
+		NoWait:      (option & LOG_NOWAIT) != 0,
+		Perror:      (option & LOG_PERROR) != 0,
+		NoFallback:  (option & LOG_NOFALLBACK) != 0,
+		Reestablish: (option & LOG_REESTABLISH) != 0,
+	}
+
+	return SetSettings(s)
+}
+
+func SetSettings(s Settings) error {
+	log.l.Lock()
+	defer log.l.Unlock()
+
+	if s.NoDelay {
+		if e := establishLogger(s); e != nil {
+			return e
+		}
+	}
+
+	log.s = s
+
+	return nil
+}
+
+func establishLogger(s Settings) error {
+	var e error
+	if s.Dial {
+		w, e := syslog.Dial(
+			s.Network,
+			s.Raddr,
+			syslog.Priority(int(s.Facility)),
+			s.Ident,
+		)
+
+		if e == nil {
+			log.w = w
+
+			return nil
+		}
+
+	} else {
+		w, e := syslog.New(
+			syslog.Priority(int(s.Facility)),
+			s.Ident,
+		)
+
+		if e == nil {
+			log.w = w
+			return nil
+		}
+	}
+
+	if s.Cons {
+		f, e := os.OpenFile(
+			"/dev/console",
+			os.O_APPEND|os.O_WRONLY,
+			0600,
+		)
+
+		if e == nil {
+			log.console = f
+
+			return nil
+		}
+	}
+
+	if s.NoFallback {
+		return e
+	} else {
+		return nil
+	}
+}
+
+func (q *syslogger) syslogFmt(priority Priority, m string) (string, error) {
+	prival := priority & Priority(FacilityMask)
+	if prival == 0 || prival > Priority(LOG_LOCAL7) {
+		prival = Priority(l.s.Facility)
+	}
+	prival = prival | (priority & SeverityMask)
+
+	// BUG fix
+	timestamp := "-"
+
+	hostname, e := os.Hostname()
+	if e != nil {
+		hostname = "-"
+	}
+
+	appname := l.appname
+
+	procid := "-"
+	if l.s.Pid {
+		procid := strconv.Itoa(os.Getpid())
+	}
+
+	msgid := l.s.Ident
+
+	// Maybe add this later?
+	structureddata := "-"
+
+	msg := m
+
+	// See RFC 5424.
+	res := fmt.Sprintf(
+		"<%d>1 %s %s %s %s %s %s %s",
+		prival,
+		timestamp,
+		hostname,
+		appname,
+		procid,
+		msgid,
+		structureddata,
+		msg,
+	)
+
+	return res, nil
+}
+
+func syslogg(priority Priority, m string) error {
 	var e error // needed for switch scope, here so syslogger won't be local
 
 	if syslogger == nil {
@@ -245,59 +292,85 @@ func Syslog(priority int, m string) (error) {
 	return e
 }
 
-func Closelog() (error) {
+func Syslog(p Priority, m interface{}) error {
+	var s string
+
+	switch m := m.(type) {
+	case string:
+		s = m
+	case fmt.Stringer:
+		s = m.String()
+	case error:
+		s = m.Error()
+	default:
+		e := errors.New(
+			fmt.Sprintf(
+				"Syslog exoects its msg argument to be one"+
+					" of a string, a fmt.Stringer, or an error,"+
+					" but a value was given to Syslog that does"+
+					" not match any of these types: %v",
+				m,
+			),
+		)
+
+		return e
+	}
+
+	return syslog(p, s)
+}
+
+func Closelog() error {
 	return syslogger.Close()
 }
 
-func Emerg(m string) (error) {
+func Emerg(m interface{}) error {
 	return Syslog(LOG_EMERG, m)
 }
 
-func Emergency(m string) (error) {
+func Emergency(m interface{}) error {
 	return Emerg(m)
 }
 
-func Alert(m string) (error) {
+func Alert(m interface{}) error {
 	return Syslog(LOG_ALERT, m)
 }
 
-func Crit(m string) (error) {
+func Crit(m interface{}) error {
 	return Syslog(LOG_CRIT, m)
 }
 
-func Critical(m string) (error) {
+func Critical(m interface{}) error {
 	return Crit(m)
 }
 
-func Err(m string) (error) {
+func Err(m interface{}) error {
 	return Syslog(LOG_ERR, m)
 }
 
-func Error(m string) (error) {
+func Error(m interface{}) error {
 	return Err(m)
 }
 
-func Warning(m string) (error) {
+func Warning(m interface{}) error {
 	return Syslog(LOG_WARNING, m)
 }
 
-func Warn(m string) (error) {
+func Warn(m interface{}) error {
 	return Warning(m)
 }
 
-func Notice(m string) (error) {
+func Notice(m interface{}) error {
 	return Syslog(LOG_NOTICE, m)
 }
 
-func Info(m string) (error) {
+func Info(m interface{}) error {
 	return Syslog(LOG_INFO, m)
 }
 
-func Information(m string) (error) {
+func Information(m interface{}) error {
 	return Info(m)
 }
 
-func Debug(m string) (error) {
+func Debug(m interface{}) error {
 	return Syslog(LOG_DEBUG, m)
 }
-
