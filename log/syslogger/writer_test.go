02d7174b83c239fd4e1a19a7fe7fbbe3e0c4c92f
Charles Southerland <charlie@stuphlabs.com>
2017-05-29 15:13:32 -0500

    Adding test for syslogger.Writer.

---
+++ log/syslogger/writer_test.go
@@ -0,0 +1,113 @@
+package syslogger
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/proidiot/gone/errors"
+	"github.com/proidiot/gone/log/pri"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type errorWriter struct {
+}
+
+func (e errorWriter) Write([]byte) (int, error) {
+	return 0, errors.New("Writing to an errorWriter")
+}
+
+func TestWriterSyslog(t *testing.T) {
+	type testCase struct {
+		inputPri       pri.Priority
+		inputMsg       interface{}
+		inputBadWriter bool
+		expectedError  bool
+		expectedBytes  []byte
+	}
+
+	tests := map[string]testCase{
+		"zero values": {
+			inputPri:      pri.Priority(0x00),
+			inputMsg:      []byte{},
+			expectedError: false,
+			expectedBytes: nil,
+		},
+		"full values": {
+			inputPri:      pri.Priority(0xFF),
+			inputMsg:      []byte{0xFF, 0xFF, 0xFF, 0xFF},
+			expectedError: true,
+		},
+		"string message": {
+			inputMsg:      "testing",
+			expectedBytes: []byte("testing"),
+		},
+		"bytes message": {
+			inputMsg:      []byte{0xC0, 0xA8, 0x00, 0x01},
+			expectedBytes: []byte{0xC0, 0xA8, 0x00, 0x01},
+		},
+		"struct message": {
+			inputMsg:      testCase{},
+			expectedError: true,
+		},
+		"string message bad writer": {
+			inputMsg:       "testing",
+			inputBadWriter: true,
+			expectedError:  true,
+		},
+		"bytes message bad writer": {
+			inputMsg:       []byte{0xC0, 0xA8, 0x00, 0x01},
+			inputBadWriter: true,
+			expectedError:  true,
+		},
+	}
+
+	for explanation, test := range tests {
+		actualBytes := new(bytes.Buffer)
+
+		var writer *Writer
+		if test.inputBadWriter {
+			writer = &Writer{
+				Writer: errorWriter{},
+			}
+		} else {
+			writer = &Writer{
+				Writer: actualBytes,
+			}
+		}
+
+		actualError := writer.Syslog(
+			test.inputPri,
+			test.inputMsg,
+		)
+
+		if test.expectedError {
+			assert.Error(
+				t,
+				actualError,
+				fmt.Sprintf(
+					"Writer test expected error for: %s",
+					explanation,
+				),
+			)
+		} else {
+			assert.NoError(
+				t,
+				actualError,
+				fmt.Sprintf(
+					"Writer test unexpected error for: %s",
+					explanation,
+				),
+			)
+		}
+
+		assert.Equal(
+			t,
+			test.expectedBytes,
+			actualBytes.Bytes(),
+			fmt.Sprintf(
+				"Writer test bytes check failure for: %s",
+				explanation,
+			),
+		)
+	}
+}
